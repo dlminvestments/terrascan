4ba424d4ee38adab9e19031d08c943258dce99b6
DLM <55211307+dlminvestments@users.noreply.github.com>
2022-01-31 19:06:24 -0500

    Delete aws.tf

--- aws.tf
+++
@@ -1,9 +0,0 @@
-module "web_server_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
-
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = "vpc-0031c3dd8812b3c7a
-
-  ingress_cidr_blocks = ["172.31.0.0/16"]
-}
